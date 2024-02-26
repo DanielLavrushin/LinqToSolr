58ce0b1191994765692d5194c7e5070057168a8f
Daniel Lavrushin <dala@milestone.dk>
2024-02-26 16:25:11 +0100

    post test

---
+++ LinqToSolr.Tests/PostTests.cs
@@ -0,0 +1,18 @@
+﻿using LinqToSolr.Extensions;
+using LinqToSolr.Tests.Models;
+
+namespace LinqToSolr.Tests
+{
+    [TestClass]
+    public class PostTests : BaseTest
+    {
+        [TestMethod]
+        public async Task SelectSimpleDynamicToListTest()
+        {
+            var docs1 = await Service.AsQueryable<SolrDocument>().AsPostMethod().Select(x => x.Name).ToListAsync();
+
+            Assert.IsNotNull(docs1, "The result should not be null");
+            Assert.IsTrue(docs1.Count > 0, "There should be at least one document");
+        }
+    }
+}
